3184f2ddbdb51500f7c889a67dbdd932a23a5793
Judy McGoldrick <jjeanius@verizon.net>
2018-07-19 15:44:27 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
 
   def self.current_user(session_hash)
-   User.find_by(session_hash["user_id"])
-
+    @user = User.find_by(session_hash["user_id"])
+    
     end
 
   def self.is_logged_in?(session_hash)
