92c77c87f9c8434de28dcdd7df39bfbfbb18ccab
Judy McGoldrick <jjeanius@verizon.net>
2018-07-19 15:56:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
 
     get '/account' do
       @current_user = User.find_by_id(session[:user_id])
+      binding.pry
         if @current_user
         erb :account
       else
